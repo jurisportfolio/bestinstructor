197e06732d774ede801f1f306a8ea191670e41c6
jurkotroll <jurko.bossetroll@gmail.com>
2019-04-19 13:08:45 +0200

    uuid was added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import User from "./pages/UserPage";
 import UsersList from "./pages/UsersListPage";
+import uuidv4 from "uuid/v4";
+
+const listOfInstructors = [
+  {id: uuidv4(), name: "Red", place: "Zieleniec"},
+  
+  {id: uuidv4(), name: "Red", place: "Zieleniec"},
+  {id: uuidv4(), name: "Red", place: "Zieleniec"},
+  {id: uuidv4(), name: "Red", place: "Zieleniec"}
+];
+
+function printInstructors (listOfInstructors) {
+  return(
+    listOfInstructors.map(({id, name, place}) =>
+    console.log(`id: ${id} name: ${name} place: ${place}`)
+    )
+  )
+}
+printInstructors(listOfInstructors);
 
 const Main = () => {
   return (
